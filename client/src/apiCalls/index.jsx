ce3e39670711e5c607f0ad62818361e879b66672
satyam mishra <satyammishraofficial2000gmail.com>
2026-05-17 23:59:11 +0530

    Updated

--- client/src/apiCalls/index.jsx
+++ client/src/apiCalls/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "https://chatting-app-uc1r.onrender.com";
+export const url = "https://quick-chat-app-qviu.onrender.com";
 
 export const axiosInstance = axios.create({
     baseURL: url,
--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors({
-   origin: "https://chatting-app-client-x539.onrender.com",
+   origin: "https://quick-chat-app-qviu.onrender.com",
    credentials: true
 }))
 
@@ -18,7 +18,7 @@ app.use(express.json({
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
      cors: {
-          origin: 'https://chatting-app-client-x539.onrender.com',
+          origin: 'https://quick-chat-app-qviu.onrender.com',
           methods: ['GET', 'POST']
      }
 });
