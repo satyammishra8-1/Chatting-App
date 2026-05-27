6f095f05c9ce4b0a62cef25d6e6acdb82ca3aa7d
satyam mishra <satyammishraofficial2000gmail.com>
2026-05-27 23:17:19 +0530

    fixed brevo sender email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,31 +1,52 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
+
     host: "smtp-relay.brevo.com",
+
     port: 587,
+
     secure: false,
+
     auth: {
+
         user: process.env.BREVO_EMAIL,
+
         pass: process.env.BREVO_PASS
 
     }
+
 });
 
 const sendEmail = async (to, subject, html) => {
+
     try {
+
         const info = await transporter.sendMail({
-            from: process.env.BREVO_EMAIL,
+
+            from: '"QuickChat" <mishrashtyam@gmail.com>',
+
             to,
+
             subject,
+
             html
+
         });
-        console.log("Email sent:", info.response);
+
+        console.log("Email sent successfully");
+
+        console.log(info);
+
     } catch (error) {
+
         console.log(
             "Email sending failed:",
-            error.message
+            error
         );
+
     }
+
 };
 
 module.exports = sendEmail;
