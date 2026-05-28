eabda825c304a754ab110fa7d6addeab7065ba5f
satyam mishra <satyammishraofficial2000gmail.com>
2026-05-29 02:03:15 +0530

    Fixed Android PWA app icon

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,15 +6,51 @@ import store from './redux/store';
 import { Provider } from 'react-redux';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
+
   <GoogleOAuthProvider clientId="327683842104-kn2g4crqdp4ooosip108lnujigb0qv5e.apps.googleusercontent.com">
+
     <Provider store={store}>
+
       <App />
+
     </Provider>
+
   </GoogleOAuthProvider>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+if ('serviceWorker' in navigator) {
+
+  window.addEventListener('load', () => {
+
+    navigator.serviceWorker
+      .register('/service-worker.js')
+
+      .then((registration) => {
+
+        console.log(
+          'Service Worker registered:',
+          registration
+        );
+
+      })
+
+      .catch((error) => {
+
+        console.log(
+          'Service Worker registration failed:',
+          error
+        );
+
+      });
+
+  });
+
+}
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA({
+
+  registerType: 'autoUpdate',
+
+  manifest: {
+
+    name: 'QuickChat',
+
+    short_name: 'QuickChat',
+
+    description: 'Realtime Chat Application',
+
+    theme_color: '#6366f1',
+
+    background_color: '#ffffff',
+
+    display: 'standalone',
+
+    orientation: 'portrait',
+
+    start_url: '/',
+
+    icons: [
+
+      {
+        src: '/pwa-192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+
+      {
+        src: '/pwa-512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+
+    ]
+
+  }
+
+})],
   server: {
     proxy: {
       "/api": {
