ee0371ef2ec7fee2f942abd6cf838bf42733cc9e
satyam mishra <satyammishraofficial2000gmail.com>
2026-05-30 00:09:54 +0530

    Adding backend deployed link to frontend

--- client/src/apiCalls/index.jsx
+++ client/src/apiCalls/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:3000";
+export const url = "https://quick-chat-app-qviu.onrender.com"
 console.log("API URL:", url);
 export const axiosInstance = axios.create({
     baseURL: url,
